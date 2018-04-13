11dacc296cdcc35c6b003b4560c38fe106269e22
INmabari <inmabari@INmabari.local>
2018-04-13 17:16:24 +0530

    First commit

---
+++ src/components/list/Pagination.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Pagination.css';
+import propTypes from 'prop-types';
+
+const Pagination = (props) => {
+    const { page, totalPages, handlePaginationClick } = props;
+    return(
+        <div className="Pagination">
+            <button 
+                className="Pagination-button"
+                onClick={() => handlePaginationClick('prev')}
+                disabled={page <= 1}
+            >
+                &larr;
+            </button>
+
+            <span className="Paginatino-info">
+                page <b>{page}</b> of <b>{totalPages}</b>
+            </span>
+            <button 
+                className="Pagination-button"
+                onClick={() => handlePaginationClick('next')}
+                disabled={page >=totalPages}
+            >
+                &rarr;
+            </button> 
+        </div>
+    );
+}
+
+Pagination.propTypes = {
+    totalPages: propTypes.number.isRequired,
+    page: propTypes.number.isRequired,
+    handlePaginationClick: propTypes.func.isRequired,
+};
+
+export default Pagination;
